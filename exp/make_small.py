5ad6218206bd00e7dcfe9c52538d0f0deddf1cfc
backonhighway <pocketsuteado@gmail.com>
2019-01-21 23:04:23 +0900

    done from home

--- exp/make_small.py
+++ exp/make_small.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
 new = pd.read_csv("../input/new_merchant_transactions.csv", nrows=1000*100)
-new.to_csv("../input/new_small.csv")
 
 old = pd.read_csv("../input/historical_transactions.csv", nrows=1000*100)
+
+train = pd.read_csv("../input/train.csv")
+
+new = pd.merge(new, train, on="card_id", how="left")
+old = pd.merge(old, train, on="card_id", how="left")
+new.to_csv("../input/new_small.csv")
 old.to_csv("../input/old_small.csv")
