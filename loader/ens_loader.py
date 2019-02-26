0121c833f331bf6577dffb8911e643bdef35fb39
backonhighway <pocketsuteado@gmail.com>
2019-02-27 00:02:12 +0900

    done for the competition I guess

--- loader/ens_loader.py
+++ loader/ens_loader.py
@@ -1,6 +1,10 @@
 def get_team_ens():
     ret_files = [
-        ("team_v63", "team_v63"),
-        ("small_team", "small_team"),
+        ("team_v63", "lgb"),
+        ("bin_team", "bin"),
+        ("no_out_team", "no_out"),
+        # ("rnd_feat_bridge", "rnd_feat"),
+        # ("small_team", "small")
+        ("rank", "rank")
     ]
     return ret_files
